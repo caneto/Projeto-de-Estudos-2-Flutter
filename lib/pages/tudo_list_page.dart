0300a5d87372861a5b5730fe79a9a8a8669a4bac
Carlos Alberto da Costa Pinto Neto <caneto@gmail.com>
2022-07-30 19:37:17 -0300

    Continuação do Estudo 2

--- lib/pages/tudo_list_page.dart
+++ lib/pages/tudo_list_page.dart
@@ -1,7 +1,17 @@
 import 'package:flutter/material.dart';
+import 'package:listadetarefas/widgets/todo_list_item.dart';
 
-class TudoListaPage extends StatelessWidget {
-  const TudoListaPage({Key? key}) : super(key: key);
+class TodoListaPage extends StatefulWidget {
+  TodoListaPage({Key? key}) : super(key: key);
+
+  @override
+  State<TodoListaPage> createState() => _TodoListaPageState();
+}
+
+class _TodoListaPageState extends State<TodoListaPage> {
+  final TextEditingController todoController = TextEditingController();
+
+  List<String> todos = [];
 
   @override
   Widget build(BuildContext context) {
@@ -14,20 +24,27 @@ class TudoListaPage extends StatelessWidget {
           children: [
             Row(
               children: [
-                const Expanded(
+                Expanded(
                   child: TextField(
-                    decoration: InputDecoration(
+                    controller: todoController,
+                    decoration: const InputDecoration(
                       border: OutlineInputBorder(),
                       labelText: "Adicione uma tarefa",
                       hintText: "Ex: Digitar dados",
                     ),
                   ),
                 ),
-                SizedBox(
+                const SizedBox(
                   width: 8,
                 ),
                 ElevatedButton(
-                  onPressed: () {},
+                  onPressed: () {
+                    String text = todoController.text;
+                    setState(() {
+                      todos.add(text);
+                    });
+                    todoController.clear();
+                  },
                   style: ElevatedButton.styleFrom(
                     primary: Color(0xff00d7f3),
                     padding: EdgeInsets.all(14),
@@ -38,6 +55,16 @@ class TudoListaPage extends StatelessWidget {
                   ),
                 )
               ],
+            ),
+            const SizedBox(height: 16,),
+            Flexible(
+              child: ListView(
+                shrinkWrap: true,
+                children: [
+                for(String todo in todos)
+                  TodoListItem()
+                ],
+              ),
             ),
             SizedBox(height: 16,),
             Row(
